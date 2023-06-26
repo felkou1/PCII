b0a1cd7ca339943563a93e08fd4222ea1656397f
felkou1 <124784350+felkou1@users.noreply.github.com>
2023-06-26 14:32:43 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+import control.controlable;
+import view.Affichage;
+import view.Carte;
+
+import java.awt.event.*;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+
+public class Main {
+    public static void main(String [] args) {
+        Affichage fenetre = new Affichage("Flower Craft");
+        
+  }
+}
